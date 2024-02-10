1ccd4c212db775741de014de4851cf98b5a3f0fd
Jean Jacques Nienaba <jjn99@outlook.fr>
2024-02-10 23:03:25 +0000

    implement authentification function(test)

--- app/src/main/java/com/example/prospectmodile/activities/AuthenticationActivity.kt
+++ app/src/main/java/com/example/prospectmodile/activities/AuthenticationActivity.kt
@@ -1,12 +1,60 @@
 package com.example.prospectmodile.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.prospectmodile.R
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputLayout
 
 class AuthenticationActivity : AppCompatActivity() {
+
+    lateinit var layoutTextInputId : TextInputLayout
+    lateinit var layoutTextInputPassword:TextInputLayout
+    lateinit var btnConnect : MaterialButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authentication)
+
+        layoutTextInputId =findViewById(R.id.layoutTextInputId)
+        layoutTextInputPassword = findViewById(R.id.layoutTextInputPassword)
+        btnConnect = findViewById(R.id.btnConnect)
+
+        btnConnect.setOnClickListener {
+
+            layoutTextInputId.isErrorEnabled = false
+            layoutTextInputPassword.isErrorEnabled = false
+
+            var id = layoutTextInputId.editText?.text.toString()
+            var pwd = layoutTextInputPassword.editText?.text.toString()
+
+
+            if(id.isEmpty() || pwd.isEmpty()){
+                if(pwd.isEmpty()){
+                    layoutTextInputPassword.error = "Mot de passe obligatoire!"
+                    layoutTextInputPassword.isErrorEnabled = true
+                }
+                if(id.isEmpty()){
+                    layoutTextInputId.error = "Email obligatoire!"
+                    layoutTextInputId.isErrorEnabled =  true
+                }
+            }else{
+                signIn(id, pwd)
+            }
+        }
+    }
+
+    fun signIn(id:String,pwd:String){
+        if(id == "gest" && pwd == "2323"){
+            Intent(this,HomeActivity::class.java).also {
+                startActivity(it)
+            }
+            finish()
+        }
+        else{
+            layoutTextInputPassword.error = "Authentification Echouer!"
+            layoutTextInputId.isErrorEnabled = true
+            layoutTextInputPassword.isErrorEnabled = true
+        }
     }
 }
